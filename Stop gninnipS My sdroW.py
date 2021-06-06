dae30463f352eb7454fddc84b5d41f1d5d016378
zmalpq <85382697+zmalpq@users.noreply.github.com>
2021-06-06 15:07:38 +1000

    Create Stop gninnipS My sdroW.py

---
+++ Stop gninnipS My sdroW.py
@@ -0,0 +1,23 @@
+#Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed
+#(like the name of this kata).
+
+#Strings passed in will consist of only letters and spaces.
+#Spaces will be included only when more than one word is present.
+
+#Examples:
+
+#spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
+#spinWords("This is a test") => "This is a test" 
+#spinWords("This is another test") => "This is rehtona test"
+
+#solution
+def spin_words(sentence):
+    # Your code goes here
+    arr = sentence.split()
+    ans = []
+    for i in arr:
+        if len(i) >= 5:
+            ans.append(i[::-1])
+        else:
+            ans.append(i)
+    return " ".join(ans)
