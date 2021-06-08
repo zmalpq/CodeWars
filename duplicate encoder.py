7bb5d86e9321ea166be1ec6e0b934c89be20f235
zmalpq <85382697+zmalpq@users.noreply.github.com>
2021-06-08 21:31:43 +1000

    Create duplicate encoder.py

---
+++ duplicate encoder.py
@@ -0,0 +1,23 @@
+#link: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/python
+
+#The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+#Examples
+#"din"      =>  "((("
+#"recede"   =>  "()()()"
+#"Success"  =>  ")())())"
+#"(( @"     =>  "))((" 
+
+#solution
+
+def duplicate_encode(word):
+    lst = []
+    word = word.lower()
+    for i in word:
+        if word.count(i) == 1: 
+            i = '('
+            lst.append(i)
+        else: 
+            i = ')'
+            lst.append(i)
+    return ''.join(lst)
